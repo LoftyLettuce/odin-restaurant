cf6c39d454ced05875d3c8648a7e9d46135c8c07
LoftyLettuce <lubolulubo2@gmail.com>
2024-09-01 14:20:58 +0700

    Add status attribute

--- src/home/home.js
+++ src/home/home.js
@@ -1,13 +1,16 @@
 import "./home.css";
+
 function create(ele ,content, parent, doc)
 {
   const child = doc.createElement(ele);
   child.textContent = content;
   parent.appendChild(child);
 }
-export const homeMake = function(doc){
-  console.log("oksdf");
+export const makeHome = function(doc){
   const content = doc.getElementById("content");
+  content.className = "home";
+  const button = doc.getElementById("home");
+  button.setAttribute("status","active");
   create("h1", "Restaurant", content, doc);
   create("div", "A normal one.", content, doc);
 }
--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import "./index.css"
-import {homeMake} from "./home/home"
-function reset(doc)
+import "./menu/menu.css"
+import { makeHome } from "./home/home"
+import { makeMenu } from "./menu/menu";
+document.getElementById("home").getAttribute
+function reset(doc, button)
 {
+  Array.from(doc.querySelector("nav").children).forEach(button => {
+    if (button.getAttribute("status") == "active")
+    {
+      button.setAttribute("status", "inactive");
+    }
+  })
   doc.body.removeChild(doc.getElementById("content"));
   const content = doc.body.appendChild(doc.createElement("div"));
   content.id = "content";
 }
 document.addEventListener("DOMContentLoaded", function(){
-  homeMake(this);
-  this.getElementById("home").addEventListener("click", function(){
-    reset(document);
-    homeMake(document);
+  makeHome(this);
+  this.getElementById("home").addEventListener("click", function(e){
+    reset(document, e.target);
+    makeHome(document);
   })
-  this.getElementById("menu").addEventListener("click", function(){
-    
+  this.getElementById("menu").addEventListener("click", function(e){
+    reset(document, e.target);
+    makeMenu(document);
   })
-  this.getElementById("about").addEventListener("click", function(){
+  this.getElementById("about").addEventListener("click", function(e){
     
   })
 })
